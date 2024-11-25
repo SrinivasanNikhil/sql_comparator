4845ed0a82b7773c0556393e75b1d7470e686dff
SrinivasanNikhil <nikhil.srinivasan@gmail.com>
2024-11-25 16:56:07 -0500

    first commit

---
+++ Assignment3.sql
@@ -0,0 +1,5 @@
+{
+    "1": "SELECT * FROM Orders",
+    "2": "SELECT COUNT(orderNumber), status FROM Orders GROUP BY status",
+    "3": "SELECT productName, buyPrice FROM Products WHERE buyPrice > 50.00"
+}
---
+++ select_from_where.sql
@@ -0,0 +1,12 @@
+{
+    "1": "SELECT firstName, lastName FROM employees WHERE jobTitle = 'Sales';",
+    "2": "SELECT productName FROM products WHERE productLine = 'Classic Cars';",
+    "3": "SELECT customerName FROM customers WHERE country = 'USA';",
+    "4": "SELECT * FROM orders WHERE customerNumber = 103;",
+    "5": "SELECT productName FROM products WHERE buyPrice > 100;",
+    "6": "SELECT * FROM orders WHERE status = 'On Hold';",
+    "7": "SELECT firstName, lastName FROM employees WHERE lastName LIKE 'B%';",
+    "8": "SELECT customerName FROM customers WHERE creditLimit < 50000;",
+    "9": "SELECT city, country FROM offices WHERE territory = 'Asia';",
+    "10": "SELECT productName FROM products WHERE quantityInStock < 500;"
+}
